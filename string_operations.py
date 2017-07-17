8bdcf0f5975fec85b7bceabd6aafa0f59cdf5a27
viinay <viinay@users.noreply.github.com>
2017-07-18 00:17:10 +0530

    Create string_operations.py

---
+++ string_operations.py
@@ -0,0 +1,10 @@
+n1 = 15
+n2 = 25
+print "maximum of "+ n1 +"and " + n2 +"is " +max(n1,n2)
+#maximum of 15 and 25 is 25
+
+print "maximum of {0} and {1} is {2}".format(n1,n2,max(n1,n2))
+
+print "maximum of %i and %i is %d".format(n1,n2,max(n1,n2))
+""" where %i and %d are placeholders for integer and digit respectively """
+
