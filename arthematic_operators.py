b7982e15d5e76ad57c64d9a2265d6ac4103a04de
viinay <viinay@users.noreply.github.com>
2017-08-06 20:56:36 +0530

    Update arthematic_operators.py

--- arthematic_operators.py
+++ arthematic_operators.py
@@ -2,3 +2,10 @@
 radius = input("enter radius (m):")
 area = 3.14*int(radius)**2
 """ input stores data as a string (>>>(type(10) != type('10')) so, trun radius into integer using `int()` this is called typecasting """
+
+
+
+import math
+pi = math.pi
+radius = input("enter radius (m):")
+area = pi*int(radius)**2
